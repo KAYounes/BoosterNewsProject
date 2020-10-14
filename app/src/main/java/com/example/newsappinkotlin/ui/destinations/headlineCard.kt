48e48541a256c413832f0f8a9eae4f163ada1b35
Kareem Amr Younes <official.kyounes@gmail.com>
2020-10-14 09:16:52 +0200

    created full news model

---
+++ app/src/main/java/com/example/newsappinkotlin/ui/destinations/headlineCard.kt
@@ -0,0 +1,18 @@
+package com.example.newsappinkotlin.ui.destinations
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsappinkotlin.R
+
+class headlineCard : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_headline_card, container, false)
+
+
+}
